3fbaf10ee2d878a050d4e034913bb0a7f23ad612
NaranjaX <hernanghernandez@gmail.com>
2024-04-08 10:51:08 -0300

    actualizo repo

--- AnalisisEspacial/script.R
+++ AnalisisEspacial/script.R
@@ -32,17 +32,71 @@ for(sexo in sexo){
   print(sexo)
 }
 
+#Armo los cluster
 
-for(sexo in sexo){
-  
-  tasas_ntd_df[tasas_ntd_df$sexo== sexo,c(15)] <- tasas_ntd_df$Ii[tasas_ntd_df$sexo== sexo]-mean(tasas_ntd_df$Ii[tasas_ntd_df$sexo== sexo])
-  tasas_ntd_df[tasas_ntd_df$sexo== sexo,c(16)] <- tasas_ntd_df$suavizadas[tasas_ntd_df$sexo== sexo]-mean(tasas_ntd_df$suavizadas[tasas_ntd_df$sexo== sexo])
-  
+tasas_ntd_df <- tasas_ntd_df %>%
+  mutate(cluster= case_when(`Pr(z != E(Ii))` > 0.05 ~ 'no significativo',
+                            TRUE ~ mean))
 
-  
-  
-  
-  print(sexo)
-}
+#Cargo la cartografia de los departamentos
 
-colnames()
+deptos <- st_read(dsn= "cartografia/deptos",
+                  layer = "pxdptodatosok")
+
+#Arreglo las geometrias
+deptos <- st_make_valid(deptos)
+
+#Cargo la cartografia de las provincias
+pcias <- st_read(dsn= "cartografia/provincias",
+                 layer = "pxpciadatosok") 
+#Arreglo las geometrias
+pcias <- st_make_valid(pcias)
+
+#Selecciono caba como provincia
+caba <- pcias %>% filter(link == '02') 
+
+
+#Excluyo antartida y caba
+deptos <- deptos %>% filter(!link %in% c(94028,94021) & codpcia != '02')
+
+#Uno los departamentos y caba
+deptos <- deptos %>%
+  bind_rows(caba)
+
+#Arreglo los datos
+
+deptos <- deptos %>%
+  mutate(link= case_when(link== '02' ~ '02000', TRUE ~ link),
+         codpcia= case_when(link== '02000' ~ '02',TRUE ~ codpcia),
+         departamen= case_when(link== '02000' ~ 'CABA',TRUE ~ departamen))
+
+#Agrego los datos de ambos sexos
+
+
+deptos <- deptos %>%
+  left_join(tasas_ntd_df %>% filter(grepl("3.",sexo)) %>% select(link,RME,suavizadas,cluster))
+
+#Armo los mapas
+
+colores_personalizados <- colorFactor(
+  palette = c("red", "blue", "lightpink", "skyblue2", "white"),
+  domain = c("High-High", "Low-Low", "High-Low", "Low-High", "no significativo")
+)
+
+
+
+leaflet(deptos) %>%
+  addTiles(urlTemplate = "https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png",
+           tileOptions(tms = TRUE,maxZoom = 14),attribution = '<a target="_blank" href="https://www.ign.gob.ar/argenmap/argenmap.jquery/docs/#datosvectoriales" style="color: black; text-decoration: underline; font-weight: normal;">Datos IGN Argentina // OpenStreetMap</a>') %>%
+  addPolygons(fill = colores_personalizados(deptos$cluster),
+              fillColor = colores_personalizados(deptos$cluster),
+              fillOpacity = 0.7,
+              stroke = TRUE,
+              weight = 2,
+              popup = paste0(deptos$departamen,"\n",deptos$cluster)) %>%
+  addLegend(position = "bottomright", # Posición de la leyenda
+            colors = c("red", "blue", "lightpink", "skyblue2", "white"), 
+            labels = c("High-High", "Low-Low", "High-Low", "Low-High", "no significativo"), 
+            title = "Cluster")
+
+
