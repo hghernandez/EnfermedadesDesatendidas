f2baedb1df0815c8d9ed3cda337ca01a575fa33f
hghernandez <hernanghernandez@gmail.com>
2024-05-26 13:31:01 -0300

    actualizo repo

--- Evaluacion/script.R
+++ Evaluacion/script.R
@@ -10,15 +10,17 @@ library(blastula)
 ruta <- getwd()
 
 email <- blastula::render_email(paste0(ruta,"/Evaluacion/ReporteEval.Rmd"))
+# 
+# create_smtp_creds_file(
+#   file = "gmail_creds",
+#   user = "hernan.hernandez@uner.edu.ar",
+#   host = "smtp.gmail.com",
+#   port = 465,
+#   use_ssl = TRUE
+# )
 
-create_smtp_creds_file(
-  file = "gmail_creds",
-  user = "hernan.hernandez@uner.edu.ar",
-  host = "smtp.gmail.com",
-  port = 465,
-  use_ssl = TRUE
-)
 
+  
 
 email %>%
   smtp_send(
